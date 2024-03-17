21a5144191495a7e5ab2386d97d951a8c19feb51
hlovatskyi <hlovatsky@gmail.com>
2024-03-17 18:38:31 +0200

    added logging

--- src/test/java/hardTests/HardTests.java
+++ src/test/java/hardTests/HardTests.java
@@ -1,11 +1,15 @@
 package hardTests;
 
 import aplication.Calculator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
+
 public class HardTests {
+    private static final Logger LOGGER = LogManager.getLogger(HardTests.class);
     Calculator calc = new Calculator();
 
     @Test
@@ -18,5 +22,6 @@ public class HardTests {
     public void addMultiParameters() {
         int actualResult = calc.addMultiParam(8, 8, 9, 11, 22);
         assertEquals(actualResult, 58);
+        LOGGER.info("Actual result is {}", actualResult);
     }
 }
